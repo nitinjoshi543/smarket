149deb3becdaa942c02e1995faea69369d1db71d
manishmasih93 <37998358+manishmasih93@users.noreply.github.com>
2018-04-02 13:45:45 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,108 @@
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+    
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#" style="font-size:25px;"><center><span style="font-size:25px;"></span>SoftTech Software</center></a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#"><center><span class="glyphicon glyphicon-home" aria-hidden="true" style="font-size:25px;"></span><p>Home</p></center><span class="sr-only">(current)</span></a></li>
+        <li><a href="#"><center><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true" style="font-size:25px;"></span><p>Purchase</p></center></a></li>
+		<li><a href="#"><center><span class="glyphicon glyphicon-paste" aria-hidden="true" style="font-size:25px;"></span><p>Sale</p></center></a></li>
+		<li><a href="#"><center><span class="glyphicon glyphicon-credit-card" aria-hidden="true" style="font-size:25px;"></span><p>Voucher</p></center></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><center><span class="glyphicon glyphicon-file" aria-hidden="true" style="font-size:25px;"></span><p>Inventory<span class="caret"></p></span></center></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Stock Journal</a></li>
+            <li><a href="#">Stock Verification</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Product Details</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Group Details</a></li>
+			<li><a href="#">Unit Details</a></li>
+          </ul>
+        </li>
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><center><span class="glyphicon glyphicon-globe" aria-hidden="true" style="font-size:25px;"></span><p>Account<span class="caret"></span></p></center></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Suppliers</a></li>
+            <li><a href="#">Customers</a></li>
+			<li><a href="#">Accounts</a></li>
+			<li><a href="#">Brokers</a></li>
+			<li><a href="#">Coupon</a></li>
+          </ul>
+        </li>
+		<li><a href="#"><center><span class="glyphicon glyphicon-duplicate" aria-hidden="true" style="font-size:25px;"></span><p>Estimation</p></center></a></li>
+		<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><center><span class="glyphicon glyphicon-wrench" aria-hidden="true" style="font-size:25px;"></span><p>Settings<span class="caret"></p></center></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Settings</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Notifications</a></li>
+			<li><a href="#">GST Extract</a></li>
+          </ul>
+        </li>
+      </ul>
+	  <ul class="nav navbar-nav navbar-right">
+	  <li class="dropdown dropdown-large">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><center><span class="glyphicon glyphicon-open-file" aria-hidden="true" style="font-size:25px;"></span><p>Reports<b class="caret"></b></p></center></a>
+				
+				<ul class="dropdown-menu dropdown-menu-large row">
+					<li class="col-sm-4">
+						<ul>
+							<li class="dropdown-header">Inventory</li>
+							<li><a href="#">Stock Book</a></li>
+							<li><a href="#">Invoice</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Monthly</a></li>
+							<li><a href="#">Summary</a></li>
+							<li><a href="#">Expire Aging</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Stock Journal</a></li>
+						</ul>
+					</li>
+					<li class="col-sm-4">
+						<ul>
+							<li class="dropdown-header">Accounts</li>
+							<li><a href="#">General Ledger</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Tax Report</a></li>
+							<li><a href="#">Brokerage Report</a></li>
+							<li><a href="#">Due Bills</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Trial Balance</a></li>
+							<li><a href="#">Profit & Loss</a></li>
+							<li><a href="#">Balance Sheet</a></li>
+						</ul>
+					</li>
+					<li class="col-sm-4">
+						<ul>
+							<li class="dropdown-header">Others</li>
+							<li><a href="#">Barcode Label</a></li>
+							<li><a href="#">Analyse</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Counter Sales Extracts</a></li>
+							<li><a href="#">Estimation Report</a></li>
+						</ul>
+					</li>
+				</ul>
+				
+			</li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><center><span class="glyphicon glyphicon-cog" aria-hidden="true" style="font-size:25px;"></span><p>Others<span class="caret"></span></p></center></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Users</a></li>
+            <li><a href="#">Change Password</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>

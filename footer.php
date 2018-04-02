26faff773ba04dde25d83126ae3bbf09bc803ef1
manishmasih93 <37998358+manishmasih93@users.noreply.github.com>
2018-04-02 14:23:34 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="container-fluid fbox" align="center">
-					<div class="row">
-							<div class="col-lg-12" style="background-color:black;"><h2><font size="2px" color="White">2018 Design and Develped by <a href="http://www.softtechsoftware.org"><font size="3px">softtech software.<a></font></h2></div>
-					</div>
-					</div>
+	<div class="row">
+	<div class="col-lg-12" style="background-color:black;"><h2><font size="2px" color="White">2018 Design and Develped by <a href="http://www.softtechsoftware.org"><font size="3px">softtech software.<a></font></h2></div>
+	</div>
+</div>
